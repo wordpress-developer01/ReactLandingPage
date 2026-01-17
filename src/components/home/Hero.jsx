9e5b239e752e7429cd5fc95788479db4c8f61f99
wordpress-developer01 <130246057+wordpress-developer01@users.noreply.github.com>
2026-01-17 15:47:36 +0300

    end hero

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { home } from '../data/dummydata'
-import { TypewriterClass } from 'typewriter-effect'
+import { Typewriter } from 'typewriter-effect'
 
 export const Hero = () => {
   return (
@@ -9,7 +9,9 @@ export const Hero = () => {
        {home.map((val, i) =>(
         <div className="heroContainer">
           <h3>{val.text}</h3>
-          <h1> <Typewriter options = {{ strings: [`${val.name}`, `${val.post}`, `${val.design}`]}}/></h1>
+          <h1> <Typewriter options = {{ strings: [`${val.name}`, `${val.post}`, `${val.design}`], autoStart: true, loop: true}}/></h1>
+          <p data-aos="fade-left">{val.desc}</p>
+          <button className='primaryBtn' data-aos='fade-up-right'>Download CV</button>
         </div>
        ))}
      </section>
