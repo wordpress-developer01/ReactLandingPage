fe268278318b5f232fca930db7e54f6e21b08214
Muratbek Nassyrov <130246057+wordpress-developer01@users.noreply.github.com>
2026-01-19 23:23:05 +0300

    Remove Footer import from Pages component

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Footer from "../common/Footer"
 import { Header } from "../common/Header"
 import { Home } from "../home/Home"
 import { About } from "./About"
